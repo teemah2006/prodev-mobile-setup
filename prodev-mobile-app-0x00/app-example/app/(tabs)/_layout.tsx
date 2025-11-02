199768d3b758a58e64030574cd1dfe6675552a01
Fatimah Oladipo <fatimaholadipo4@gmail.com>
2025-11-02 14:11:46 +0100

    first mobile app

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { HapticTab } from '@/app-example/components/haptic-tab';
+import { IconSymbol } from '@/app-example/components/ui/icon-symbol';
+import { Colors } from '@/app-example/constants/theme';
+import { useColorScheme } from '@/app-example/hooks/use-color-scheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
